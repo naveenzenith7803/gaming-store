191dfbf17461c8dfddbbd12287f648a25644f637
naveenzenith7803 <224977@student.annauniv.edu>
2024-10-08 14:01:57 +0530

    'workingCommit'

--- Backend_Spring/src/main/java/com/gamezone/Backend/Repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gamezone.Backend.Repository;
-
-import com.gamezone.Backend.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
